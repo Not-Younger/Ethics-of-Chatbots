582ac58c9aff5add38f04bef3ae236b72a8280dc
Jonathan Young <jonathanyoung7718@gmail.com>
2024-05-01 11:41:24 -0500

    built a multiple choice section

--- index.js
+++ index.js
@@ -1,33 +1,73 @@
 document.getElementById('year').innerHTML = new Date().getFullYear();
 
-document.getElementById('user-button').style.backgroundColor = 'grey';
-document.getElementById('customer-content').style.display = 'block';
-document.getElementById('business-content').style.display = 'none';
-document.getElementById('law-maker-content').style.display = 'none';
+const userButton = document.getElementById('user-button');
+const businessButton = document.getElementById('business-button');
+const lawMakerButton = document.getElementById('law-maker-button');
+
+const customerContent = document.getElementById('customer-content');
+const businessContent = document.getElementById('business-content');
+const lawMakerContent = document.getElementById('law-maker-content');
+
+// Question 1
+const questionContainerQ1 = document.getElementById('question-container-q1');
+const Q1A = document.getElementById('q1a');
+const Q1B = document.getElementById('q1b');
+const Q1C = document.getElementById('q1c');
+const Q1D = document.getElementById('q1d');
+
+function resetButtonColorsQ1() {
+  Q1A.style.backgroundColor = 'black';
+  Q1B.style.backgroundColor = 'black';
+  Q1C.style.backgroundColor = 'black';
+  Q1D.style.backgroundColor = 'black';
+}
+
+Q1A.addEventListener('click', () => {
+  resetButtonColorsQ1();
+  Q1A.style.backgroundColor = 'green';
+});
+Q1B.addEventListener('click', () => {
+  resetButtonColorsQ1(); 
+  Q1B.style.backgroundColor = 'red';
+});
+Q1C.addEventListener('click', () => {
+  resetButtonColorsQ1();
+  Q1C.style.backgroundColor = 'red';
+});
+Q1D.addEventListener('click', () => {
+  resetButtonColorsQ1();
+  Q1D.style.backgroundColor = 'red';
+});
+
+
+userButton.style.backgroundColor = 'grey';
+customerContent.style.display = 'block';
+businessContent.style.display = 'none';
+lawMakerContent.style.display = 'none';
 
 function showCustomerContent() {
-  document.getElementById('user-button').style.backgroundColor = 'grey';
-  document.getElementById('business-button').style.backgroundColor = 'black';
-  document.getElementById('law-maker-button').style.backgroundColor = 'black';
-  document.getElementById('customer-content').style.display = 'block';
-  document.getElementById('business-content').style.display = 'none';
-  document.getElementById('law-maker-content').style.display = 'none';
+  userButton.style.backgroundColor = 'grey';
+  businessButton.style.backgroundColor = 'black';
+  lawMakerButton.style.backgroundColor = 'black';
+  customerContent.style.display = 'block';
+  businessContent.style.display = 'none';
+  lawMakerContent.style.display = 'none';
 };
 
 function showBusinessContent() {
-  document.getElementById('user-button').style.backgroundColor = 'black';
-  document.getElementById('business-button').style.backgroundColor = 'grey';
-  document.getElementById('law-maker-button').style.backgroundColor = 'black';
-  document.getElementById('customer-content').style.display = 'none';
-  document.getElementById('business-content').style.display = 'block';
-  document.getElementById('law-maker-content').style.display = 'none';
+  userButton.style.backgroundColor = 'black';
+  businessButton.style.backgroundColor = 'grey';
+  lawMakerButton.style.backgroundColor = 'black';
+  customerContent.style.display = 'none';
+  businessContent.style.display = 'block';
+  lawMakerContent.style.display = 'none';
 };
 
 function showLawMakerContent() {
-  document.getElementById('user-button').style.backgroundColor = 'black';
-  document.getElementById('business-button').style.backgroundColor = 'black';
-  document.getElementById('law-maker-button').style.backgroundColor = 'grey';
-  document.getElementById('customer-content').style.display = 'none';
-  document.getElementById('business-content').style.display = 'none';
-  document.getElementById('law-maker-content').style.display = 'block';
+  userButton.style.backgroundColor = 'black';
+  businessButton.style.backgroundColor = 'black';
+  lawMakerButton.style.backgroundColor = 'grey';
+  customerContent.style.display = 'none';
+  businessContent.style.display = 'none';
+  lawMakerContent.style.display = 'block';
 };
