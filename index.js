f17d6fc6f92bfbd2d3facd082c5de92384b3cc33
Jonathan Young <jonathanyoung7718@gmail.com>
2024-04-30 22:12:05 -0500

    dynamic page rendering lol

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 document.getElementById('year').innerHTML = new Date().getFullYear();
+
+document.getElementById('user-button').style.backgroundColor = 'grey';
+document.getElementById('customer-content').style.display = 'block';
+document.getElementById('business-content').style.display = 'none';
+document.getElementById('law-maker-content').style.display = 'none';
+
+function showCustomerContent() {
+  document.getElementById('user-button').style.backgroundColor = 'grey';
+  document.getElementById('business-button').style.backgroundColor = 'black';
+  document.getElementById('law-maker-button').style.backgroundColor = 'black';
+  document.getElementById('customer-content').style.display = 'block';
+  document.getElementById('business-content').style.display = 'none';
+  document.getElementById('law-maker-content').style.display = 'none';
+};
+
+function showBusinessContent() {
+  document.getElementById('user-button').style.backgroundColor = 'black';
+  document.getElementById('business-button').style.backgroundColor = 'grey';
+  document.getElementById('law-maker-button').style.backgroundColor = 'black';
+  document.getElementById('customer-content').style.display = 'none';
+  document.getElementById('business-content').style.display = 'block';
+  document.getElementById('law-maker-content').style.display = 'none';
+};
+
+function showLawMakerContent() {
+  document.getElementById('user-button').style.backgroundColor = 'black';
+  document.getElementById('business-button').style.backgroundColor = 'black';
+  document.getElementById('law-maker-button').style.backgroundColor = 'grey';
+  document.getElementById('customer-content').style.display = 'none';
+  document.getElementById('business-content').style.display = 'none';
+  document.getElementById('law-maker-content').style.display = 'block';
+};
