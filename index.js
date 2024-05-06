1a24a103f6e755b11a6de2c3c5b577ff3b520dc4
Jonathan Young <jonathanyoung7718@gmail.com>
2024-05-05 19:34:44 -0500

    started refactor of multiple choice buttons

--- index.js
+++ index.js
@@ -8,36 +8,49 @@ const customerContent = document.getElementById('customer-content');
 const businessContent = document.getElementById('business-content');
 const lawMakerContent = document.getElementById('law-maker-content');
 
-// Question 1
-const questionContainerQ1 = document.getElementById('question-container-q1');
-const Q1A = document.getElementById('q1a');
-const Q1B = document.getElementById('q1b');
-const Q1C = document.getElementById('q1c');
-const Q1D = document.getElementById('q1d');
+// Questions
+function rightAnswer(q, but, ans) {
+  const questionContainer = document.getElementById(q);
+  const button = document.getElementById(but);
 
-function resetButtonColorsQ1() {
-  Q1A.style.backgroundColor = 'black';
-  Q1B.style.backgroundColor = 'black';
-  Q1C.style.backgroundColor = 'black';
-  Q1D.style.backgroundColor = 'black';
+  if (ans == 'right') {
+    button.classList.add('right-answer');
+  }
+  else {
+    button.classList.add('wrong-answer');
+  }
 }
 
-Q1A.addEventListener('click', () => {
-  resetButtonColorsQ1();
-  Q1A.style.backgroundColor = 'green';
-});
-Q1B.addEventListener('click', () => {
-  resetButtonColorsQ1(); 
-  Q1B.style.backgroundColor = 'red';
-});
-Q1C.addEventListener('click', () => {
-  resetButtonColorsQ1();
-  Q1C.style.backgroundColor = 'red';
-});
-Q1D.addEventListener('click', () => {
-  resetButtonColorsQ1();
-  Q1D.style.backgroundColor = 'red';
-});
+// Question 1
+// const questionContainerQ1 = document.getElementById('question-container-q1');
+// const Q1A = document.getElementById('q1a');
+// const Q1B = document.getElementById('q1b');
+// const Q1C = document.getElementById('q1c');
+// const Q1D = document.getElementById('q1d');
+
+// function resetButtonColorsQ1() {
+//   Q1A.style.backgroundColor = 'black';
+//   Q1B.style.backgroundColor = 'black';
+//   Q1C.style.backgroundColor = 'black';
+//   Q1D.style.backgroundColor = 'black';
+// }
+
+// Q1A.addEventListener('click', () => {
+//   resetButtonColorsQ1();
+//   Q1A.style.backgroundColor = 'green';
+// });
+// Q1B.addEventListener('click', () => {
+//   resetButtonColorsQ1(); 
+//   Q1B.style.backgroundColor = 'red';
+// });
+// Q1C.addEventListener('click', () => {
+//   resetButtonColorsQ1();
+//   Q1C.style.backgroundColor = 'red';
+// });
+// Q1D.addEventListener('click', () => {
+//   resetButtonColorsQ1();
+//   Q1D.style.backgroundColor = 'red';
+// });
 
 // Question 2
 const questionContainerQ2 = document.getElementById('question-container-q2');
